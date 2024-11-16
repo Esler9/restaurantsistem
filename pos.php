6375fd6d0e50a680a9ac8832b5039d3b9b3f641f
Esler Escobar <escobarcarpio@gmail.com>
2024-11-15 20:35:34 -0600

    ancho

--- pos.php
+++ pos.php
@@ -10,7 +10,7 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
     <!-- Navbar -->
@@ -62,19 +62,10 @@
 
     <!-- Content Wrapper -->
     <div class="content-wrapper">
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Punto de Venta</h1>
-                    </div>
-                </div>
-            </div>
-        </section>
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col">
                         <div class="card">
                             <div class="card-header">
                                 <h3 class="card-title">Menú de Productos</h3>
@@ -83,7 +74,7 @@
                                 </div>
                             </div>
                             <div class="card-body">
-                                <table class="table table-striped" id="product-table">
+                                <table class="table table-striped table-bordered" id="product-table">
                                     <thead>
                                         <tr>
                                             <th>ID</th>
