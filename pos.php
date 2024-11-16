04588163c8842b1b6bb5e444ecb6241769f6c2d1
Esler Escobar <escobarcarpio@gmail.com>
2024-11-15 20:34:03 -0600

    styles

--- pos.php
+++ pos.php
@@ -6,7 +6,7 @@
     <title>Punto de Venta | Restaurante</title>
     <!-- AdminLTE -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="stylespos.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
