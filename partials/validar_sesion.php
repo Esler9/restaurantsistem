11ae0b874be606e3662d2b9e47ccccedc66b2fe6
Esler Escobar <escobarcarpio@gmail.com>
2024-12-13 16:57:31 -0600

    id user

--- partials/validar_sesion.php
+++ partials/validar_sesion.php
@@ -2,12 +2,13 @@
 session_start();
 
 // Verificar si el usuario ha iniciado sesión
-if (!isset($_SESSION['usuario'])) {
+if (!isset($_SESSION['usuario_id'])) {
     // Redirigir al login si no hay sesión activa
+
     header('Location: /login');
     exit;
 }
 
 // Almacenar datos del usuario logueado
-$usuario = $_SESSION['usuario'];
+$usuario_id = $_SESSION['usuario_id'];
 ?>
